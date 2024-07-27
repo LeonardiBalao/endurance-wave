cd81eb20f0c6d311b37b110c7f00433e9ecee4bc
Balao <matheus.balao@hotmail.com>
2024-07-27 19:34:48 -0300

    Fix auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import Main from "@/components/structural/main";
 import Nav from "@/components/structural/navigation/nav";
+import { auth } from "@/server/auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
   return (
     <>
       <Nav />
       <Main>
-        <div>alo</div>
+        <div>
+          <pre>{JSON.stringify(session)}</pre>
+        </div>
       </Main>
     </>
   );

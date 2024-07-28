61a5fca181fb17c28c253c567cdb0bd88a9f7544
Balao <matheus.balao@hotmail.com>
2024-07-28 14:31:36 -0300

    Add images and basic LP

--- app/(home)/HomeCarousel.tsx
+++ app/(home)/HomeCarousel.tsx
@@ -83,6 +83,7 @@ export function HomeCarousel({ className }: HomeCarousel) {
                       alt="Image"
                       className="rounded-sm object-cover"
                       fill
+                      unoptimized
                     />
                   </AspectRatio>
                 </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     <html lang="en-US" suppressHydrationWarning>
       <body
         className={cn(
-          "flex min-h-screen w-full flex-col bg-background",
+          "flex min-h-svh w-full flex-col bg-background",
           fontSans.className
         )}
       >

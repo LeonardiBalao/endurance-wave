af797be3b0ed6bd211d2981ffb2e20f319ff0cb7
Balao <matheus.balao@hotmail.com>
2024-08-07 20:02:04 -0300

    Add db push

--- server/actions/category/create-review.ts
+++ server/actions/category/create-review.ts
@@ -22,64 +22,68 @@ interface ReviewProps {
 }
 
 export const createReview = async (review: ReviewProps) => {
-  if (!review.userId) return { error: "No user ID" };
-  if (!review.category || !review.subcategory)
-    return { error: "No category and subcategory" };
-  if (
-    !review.comparative ||
-    !review.introduction ||
-    !review.conclusion ||
-    !review.title
-  )
-    return { error: "No main text or title." };
-  if (
-    !review.mainImageALT ||
-    !review.mainImageURL ||
-    !review.comparativeImageURL ||
-    !review.comparativeImageALT ||
-    !review.introductionImageURL ||
-    !review.introductionImageALT
-  )
-    return { error: "Missing image property" };
-  if (!review.keywords || !review.tags) return { error: "No keywords or tags" };
+  try {
+    if (!review.userId) return { error: "No user ID" };
+    if (!review.category || !review.subcategory)
+      return { error: "No category and subcategory" };
+    if (
+      !review.comparative ||
+      !review.introduction ||
+      !review.conclusion ||
+      !review.title
+    )
+      return { error: "No main text or title." };
+    if (
+      !review.mainImageALT ||
+      !review.mainImageURL ||
+      !review.comparativeImageURL ||
+      !review.comparativeImageALT ||
+      !review.introductionImageURL ||
+      !review.introductionImageALT
+    )
+      return { error: "Missing image property" };
+    if (!review.keywords || !review.tags)
+      return { error: "No keywords or tags" };
 
-  const category = await prisma.category.findFirst({
-    where: {
-      name: review.category,
-    },
-  });
+    const category = await prisma.category.findFirst({
+      where: {
+        name: review.category,
+      },
+    });
 
-  if (!category) return { error: "No category" };
+    if (!category) return { error: "No category" };
 
-  const subcategory = await prisma.subcategory.findFirst({
-    where: {
-      categoryId: category.id,
-      name: review.subcategory,
-    },
-  });
+    const subcategory = await prisma.subcategory.findFirst({
+      where: {
+        categoryId: category.id,
+        name: review.subcategory,
+      },
+    });
 
-  if (!subcategory) return { error: "No subcategory" };
+    if (!subcategory) return { error: "No subcategory" };
 
-  await prisma.review.create({
-    data: {
-      userId: review.userId,
-      title: review.title,
-      categoryId: category.id,
-      subcategoryId: subcategory.id,
-      description: review.description,
-      introduction: review.introduction,
-      comparative: review.comparative,
-      conclusion: review.conclusion,
-      comparativeImageALT: review.comparativeImageALT,
-      comparativeImageURL: review.comparativeImageURL,
-      mainImageURL: review.mainImageURL,
-      introductionImageALT: review.introductionImageALT,
-      introductionImageURL: review.introductionImageURL,
-      mainImageALT: review.mainImageALT,
-      tags: review.tags.split(","),
-      keywords: review.keywords.split(","),
-    },
-  });
-
-  return { success: `Review "${review.title}" created.` };
+    await prisma.review.create({
+      data: {
+        userId: review.userId,
+        title: review.title,
+        categoryId: category.id,
+        subcategoryId: subcategory.id,
+        description: review.description,
+        introduction: review.introduction,
+        comparative: review.comparative,
+        conclusion: review.conclusion,
+        comparativeImageALT: review.comparativeImageALT,
+        comparativeImageURL: review.comparativeImageURL,
+        mainImageURL: review.mainImageURL,
+        introductionImageALT: review.introductionImageALT,
+        introductionImageURL: review.introductionImageURL,
+        mainImageALT: review.mainImageALT,
+        tags: review.tags.split(","),
+        keywords: review.keywords.split(","),
+      },
+    });
+    return { success: `Review "${review.title}" created.` };
+  } catch (err) {
+    return { error: JSON.stringify(err) };
+  }
 };

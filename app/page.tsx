308445fe120f2db7266d29a4006325effd7c9ba7
Balao <matheus.balao@hotmail.com>
2024-08-08 20:31:18 -0300

    Add MegaMenu

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Main from "@/components/structural/main";
 import prisma from "@/server/db";
 import ReviewElement from "@/components/posts/review";
 import Nav from "@/components/structural/navigation/nav";
-import NavMenu from "@/components/structural/navigation/nav-menu";
 
 export default async function Dashboard() {
   const reviews = await prisma.review.findMany();
